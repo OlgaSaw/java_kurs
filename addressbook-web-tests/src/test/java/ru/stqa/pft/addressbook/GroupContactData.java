ffa9b4954df6996f022089f55981af7ead0af37d
OlgaSaw <olga.nowakowska@op.pl>
2018-11-30 19:46:13 +0100

    Dodanie klasy CreateGroupData

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupContactData.java
@@ -0,0 +1,109 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String mobile;
+  private final String email;
+  private final String homepage;
+  private final String bday;
+  private final String bmonth;
+  private final String byear;
+  private final String aday;
+  private final String amonth;
+  private final String ayear;
+  private final String newgroup;
+
+  public GroupContactData(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String mobile, String email, String homepage, String bday, String bmonth, String byear, String aday, String amonth, String ayear, String newgroup) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.mobile = mobile;
+    this.email = email;
+    this.homepage = homepage;
+    this.bday = bday;
+    this.bmonth = bmonth;
+    this.byear = byear;
+    this.aday = aday;
+    this.amonth = amonth;
+    this.ayear = ayear;
+    this.newgroup = newgroup;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+
+  public String getBday() {
+    return bday;
+  }
+
+  public String getBmonth() {
+    return bmonth;
+  }
+
+  public String getByear() {
+    return byear;
+  }
+
+  public String getAday() {
+    return aday;
+  }
+
+  public String getAmonth() {
+    return amonth;
+  }
+
+  public String getAyear() {
+    return ayear;
+  }
+
+  public String getNewgroup() {
+    return newgroup;
+  }
+}
