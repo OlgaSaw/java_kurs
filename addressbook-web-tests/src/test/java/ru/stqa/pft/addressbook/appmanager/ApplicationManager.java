3beb9565ebea8a2e344eea9591685ae627e5acdb
OlgaSaw <olga.nowakowska@op.pl>
2018-12-07 06:42:55 +0100

    Wybór różnych przeglądarek

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,25 +2,43 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    }
+    if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
+
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost:8081/addressbook");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    sessionHelper= new SessionHelper(wd);
+    sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
     sessionHelper.login("admin", "secret");
   }
@@ -37,7 +55,6 @@ public class ApplicationManager {
       return false;
     }
   }
-
 
 
   public void logout() {
@@ -57,4 +74,7 @@ public class ApplicationManager {
   }
 
 
-}
+  }
+
+
+
